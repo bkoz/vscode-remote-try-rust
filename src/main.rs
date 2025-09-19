ad590cd312178e10aab46a1bf0fa9aebbc7b299f
Bob Kozdemba <bkozdemba@gmail.com>
2025-09-19 03:52:20 +0000

    Update Cargo.toml to specify edition and refactor main.rs to include add function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-fn main() {
+fn add(x: u32, y: u32) -> u32 {
+    return x + y;
+}
+ fn main() {
     let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    println!("Hello, {} {}!", name, add(2, 3));
 }
